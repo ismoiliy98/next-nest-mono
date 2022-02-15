9ea3f38888abdcf169da20b9053c0a7e543331a0
bek <geebrox@gmail.com>
2022-02-15 12:26:41 +0500

    feat: add a11y related things & use swc for client build & add more configs to .swcrc in server

--- workspaces/client/next.config.js
+++ workspaces/client/next.config.js
@@ -5,8 +5,13 @@ const appPort = (parseInt(PORT, 10) || 3000) + 1;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     externalDir: true,
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   async rewrites() {
     return {
--- workspaces/client/pages/_app.tsx
+++ workspaces/client/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import '@client/assets/styles/tailwind.scss';
 import type { AppContext, AppProps } from 'next/app';
 import App from 'next/app';
+import Head from 'next/head';
 
 interface NextAppProps extends AppProps {
   appKey: string;
@@ -10,6 +11,11 @@ interface NextAppProps extends AppProps {
 const NextApp = ({ Component, pageProps, appKey }: NextAppProps) => {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Next.js application</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Next.js and Nest.js monorepo" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
