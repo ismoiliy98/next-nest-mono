5290a09b19794cc7abc5b75de56f68899a9b090e
bek <geebrox@gmail.com>
2022-05-20 13:08:42 +0500

    feat: add logger service

--- workspaces/server/src/main.ts
+++ workspaces/server/src/main.ts
@@ -1,5 +1,5 @@
 import fastifyCookie from '@fastify/cookie';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import type { ConfigType } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import type { NestFastifyApplication as App } from '@nestjs/platform-fastify';
@@ -11,29 +11,23 @@ import { PrismaService } from '@server/prisma.service';
 declare const module: any;
 
 (async () => {
-  const app = await NestFactory.create<App>(AppModule, new Adapter());
+  const app = await NestFactory.create<App>(AppModule, new Adapter(), {
+    logger: new ConsoleLogger('APP', { logLevels: ['error', 'warn', 'debug'] }),
+  });
   const appConfig = app.get<ConfigType<typeof AppConfig>>(AppConfig.KEY);
   const { appPort, appSecret } = appConfig;
 
   app
-    .useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-        whitelist: true,
-      })
-    )
+    .useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
     .setGlobalPrefix('/api')
-    .register(fastifyCookie, {
-      secret: appSecret,
-      prefix: '__Host-',
-    });
+    .register(fastifyCookie, { secret: appSecret, prefix: '__Host-' });
 
   const prismaService: PrismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
   await app.listen(appPort, '0.0.0.0');
   const mode = process.env.NODE_ENV || 'development';
-  Logger.log(
+  Logger.debug(
     `🚀 Server is running in ${mode} mode on port ${appPort}`,
     'Bootstrap'
   );
